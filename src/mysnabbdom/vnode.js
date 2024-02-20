f866c976a6562a611452d52ed7c00e6b018e92c3
feiniujuanjuan <1724995247@qq.com>
2024-02-20 18:30:41 +0800

    diff算法

--- src/mysnabbdom/vnode.js
+++ src/mysnabbdom/vnode.js
@@ -1,5 +1,5 @@
 // 作用: 创建虚拟节点
-export default function (sel, data, children, text, elm) {
+export default function Vnode(sel, data, children, text, elm) {
   const key = data.key;
   return { sel, data, children, text, elm, key }
 }
